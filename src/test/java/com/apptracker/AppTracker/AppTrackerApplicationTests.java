ca0d88cdfb81b888b8c97f78cf972b817ceca3e3
Adam Hudson <adamnubbz@gmail.com>
2023-05-19 09:01:04 -0500

    initial commit

---
+++ src/test/java/com/apptracker/AppTracker/AppTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apptracker.AppTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
